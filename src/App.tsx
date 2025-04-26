75e6a0d03967b2faf05ef459fcfaf6a11d886cc4
Nathanael Sumpter <nsumpter98@gmail.com>
2025-04-26 15:45:13 -0400

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <p>Sumpter Software LLC</p>
+      <h1 className="text-3xl font-bold underline">Sumpter Software LLC</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
