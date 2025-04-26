16603df59c376744b0dd29a762ed0a6aa544f363
Nathanael Sumpter <nsumpter98@gmail.com>
2025-04-26 16:27:11 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import { Navbar } from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="flex items-center justify-center min-h-screen bg-white">
+        <div className="border-2 px-8 py-4 rounded-2xl">
+          <h1 className="text-3xl md:text-5xl font-light tracking-wide text-gray-800 text-center">
+            Sumpter Software LLC
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
