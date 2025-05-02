ab77edff045dae4a571210dbf657bd853acfd745
Nathanael Sumpter <nsumpter98@gmail.com>
2025-05-02 14:56:52 -0400

    style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ function App() {
     <div className="relative min-h-screen bg-black">
       {/* Background Section */}
       <div
-        className="absolute inset-0 bg-cover bg-center filter blur-lg"
+        className="absolute inset-0 bg-cover bg-center filter blur-lg lg:bg-contain lg:bg-no-repeat"
         style={{
-          backgroundImage: `url(${myImage})`, // Add your image path
+          backgroundImage: `url(${myImage})`,
         }}
       ></div>
 
       {/* Foreground Logo */}
       <div className="relative flex items-center justify-center min-h-screen">
         <div className="px-8 py-6 rounded-2xl bg-gradient-to-r from-white/20 to-white/10 backdrop-blur-md border border-white/30 shadow-lg">
-          <h1 className="text-4xl md:text-6xl font-extrabold font-mono tracking-wider bg-clip-text bg-gradient-to-r text-white">
+          <h1 className="sm:text-3xl md:text-4xl font-extrabold font-mono tracking-wider bg-clip-text bg-gradient-to-r text-white">
             Sumpter Software LLC
           </h1>
         </div>
